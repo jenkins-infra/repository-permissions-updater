062238f6cfdd61517c774de0d07c8a6bb9699d71
TheMeinerLP <6745190+TheMeinerLP@users.noreply.github.com>
2024-07-27 16:59:11 +0200

    Cleanup old classes

--- src/main/java/io/jenkins/infra/repository_permissions_updater/hosting/Verifier.java
+++
@@ -1,8 +0,0 @@
-package io.jenkins.infra.repository_permissions_updater.hosting;
-
-import java.io.IOException;
-import java.util.HashSet;
-
-public interface Verifier {
-    void verify(HostingRequest request, HashSet<VerificationMessage> hostingIssues) throws IOException;
-}
