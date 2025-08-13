05a866794103f422ce333e6dd7db0139b641ac90
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-08-13 16:44:04 +0200

    Update run arguments in ArtifactoryPermissionsUpdaterRun for improved directory structure

--- src/test/java/io/jenkins/infra/repository_permissions_updater/run/ArtifactoryPermissionsUpdaterRun.java
+++ src/test/java/io/jenkins/infra/repository_permissions_updater/run/ArtifactoryPermissionsUpdaterRun.java
@@ -3,7 +3,7 @@ package io.jenkins.infra.repository_permissions_updater.run;
 class ArtifactoryPermissionsUpdaterRun {
     public static void main(String[] args) throws Exception {
         System.setProperty("dryRun", "true");
-        String[] runArgs = {"--dry-run", "--definitionsDir=temp", "--artifactoryApiTempDir=temp"};
+        String[] runArgs = {"--dry-run", "--definitionsDir=./temp/def", "--artifactoryApiTempDir=./temp/artiifacts"};
         ArtifactoryPermissionsUpdater.main(runArgs);
     }
 }
