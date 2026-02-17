5406bb4c0847085716d24c486288d0f612695238
Markus Winter <m.winter@sap.com>
2026-02-17 23:48:06 +0100

    re-add the Jira users check (#4943)

--- src/main/java/io/jenkins/infra/repository_permissions_updater/hosting/JenkinsProjectUserVerifier.java
+++ src/main/java/io/jenkins/infra/repository_permissions_updater/hosting/JenkinsProjectUserVerifier.java
@@ -17,12 +17,21 @@ public class JenkinsProjectUserVerifier implements Verifier {
             String missingInArtifactory = jenkinsProjectUsers.stream()
                     .filter(user -> !KnownUsers.existsInArtifactory(user))
                     .collect(Collectors.joining(", "));
+            String missingInJira = jenkinsProjectUsers.stream()
+                    .filter(user -> !KnownUsers.existsInJira(user))
+                    .collect(Collectors.joining(", "));
 
             if (StringUtils.isNotBlank(missingInArtifactory)) {
                 hostingIssues.add(new VerificationMessage(
                         VerificationMessage.Severity.REQUIRED,
                         "The following usernames in 'Jenkins project users to have release permission' need to log into [Artifactory](https://repo.jenkins-ci.org/): %s (reports are re-synced hourly, wait to re-check for a bit after logging in)",
                         missingInArtifactory));
+            }
+            if (StringUtils.isNotBlank(missingInJira)) {
+                hostingIssues.add(new VerificationMessage(
+                        VerificationMessage.Severity.REQUIRED,
+                        "The following usernames in 'Jenkins project users to have release permission' need to log into [Jira](https://issues.jenkins.io): %s (reports are re-synced hourly, wait to re-check for a bit after logging in)",
+                        missingInJira));
             }
         } else {
             hostingIssues.add(new VerificationMessage(
