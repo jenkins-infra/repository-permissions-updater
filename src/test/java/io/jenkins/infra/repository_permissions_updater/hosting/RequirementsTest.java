a7a2018b62ced74e677528a24b582d065dc58dbe
Markus Winter <m.winter@sap.com>
2026-07-18 15:52:09 +0200

    add test that validates the parent pom
    avoids that we add the full version in the suffix when updating parent
    pom.

---
+++ src/test/java/io/jenkins/infra/repository_permissions_updater/hosting/RequirementsTest.java
@@ -0,0 +1,15 @@
+package io.jenkins.infra.repository_permissions_updater.hosting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class RequirementsTest {
+
+    @Test
+    void validateLowestParentPom() {
+        String suffix = Requirements.LOWEST_PARENT_POM_VERSION.suffix();
+        String[] segments = suffix.split("\\.");
+        assertThat(segments.length).isEqualTo(2);
+    }
+}
