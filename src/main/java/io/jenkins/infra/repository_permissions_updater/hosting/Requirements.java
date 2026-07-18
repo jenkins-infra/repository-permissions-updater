df98f777a4307c27854e4f4e03f150164dac838b
Markus Winter <m.winter@sap.com>
2026-07-18 13:40:25 +0100

    increase minimum Jenkins to 2.541.3 (#5162)

--- src/main/java/io/jenkins/infra/repository_permissions_updater/hosting/Requirements.java
+++ src/main/java/io/jenkins/infra/repository_permissions_updater/hosting/Requirements.java
@@ -6,8 +6,8 @@ import java.util.List;
  * Requirements for hosting Jenkins plugins repositories.
  */
 public class Requirements {
-    public static final Version LOWEST_PARENT_POM_VERSION = new Version(6, "2152.ve00a_731c3ce9");
+    public static final Version LOWEST_PARENT_POM_VERSION = new Version(6, "6.2211.v27f680c93c53");
     public static final Version PARENT_POM_WITH_JENKINS_VERSION = new Version(2);
-    public static final Version LOWEST_JENKINS_VERSION = new Version(2, 528, 3);
+    public static final Version LOWEST_JENKINS_VERSION = new Version(2, 541, 3);
     public static final List<Integer> ALLOWED_JDK_VERSIONS = List.of(21, 25);
 }
